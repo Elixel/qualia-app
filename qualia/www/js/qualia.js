708adf03f02c3f8414e6449ebedea7d38875abd0
Christian Cook <christian@elixel.co.uk>
2013-08-15 12:36:16 +0100

    Integrated Intel's App Framework 2.0

--- qualia/www/js/qualia.js
+++ qualia/www/js/qualia.js
@@ -1,5 +1,7 @@
-var u;
-var ak;
+const webEngine = "http://qualia.org.uk/api/v1/";   // Qualia Web Engine URL
+
+var u;                                              // API Username
+var ak;                                             // API Key
 
 var qualia = {
     // Application Constructor
@@ -7,13 +9,11 @@ var qualia = {
         u = username;
         ak = apikey;
     },
+    // API Explorer 
+    // e.g. .api("visitor/{id}/", "GET", {}, callback)
     api: function(request, type, data, callback) {
         //If sending a POST to the API, convert the data JSON
-        if (type == "POST"){
-            data = JSON.stringify(data);
-        }
-
-        var url = "http://qualia.org.uk/api/v1/";
+        if (type == "POST") data = JSON.stringify(data);
         jQuery.ajax({
             dataType:       "json",
             async:          true,
@@ -21,23 +21,20 @@ var qualia = {
             type:           type,
             data:           data,
             contentType:    "application/json",
-            url:            url+request+"/?api_key="+ak+"&username="+u,
+            url:            webEngine+request+"/?api_key="+ak+"&username="+u,
             success:        callback,
-            error:          function(a,b,c) { console.log(b); callback(c); },
-            beforeSend:     function(jqXHR, settings) {
-                console.log(settings);
-            }
+            error:          function(a,b,c) { console.log(b); callback(c); }
         });
     },
+    // User Login
+    // Returns Visitor "id" as Integer
     login: function(email_address, callback) {
         var that = this;
         this.api("visitor","GET",{email:email_address},function(e) {
-            console.log(e);
             if (e.meta.total_count > 0) { // email does exist
                 callback(e.objects[0].id);
             } else { // email doesnt exist
                 that.api("visitor","POST",{email:email_address},function(e,t,r) {
-                    console.log(e);
                     callback(e.id);
                 });
             }
